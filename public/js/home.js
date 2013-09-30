e51eaf634ff78057d7ab31182a4d3517a0027105
thanh <thanh@hdvietnam.com.vn>
2013-09-30 14:20:49 +0700

    + cập nhật basepath để đưa lên host + fix lỗi addaction trong register

--- public/js/home.js
+++ public/js/home.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    console.log("Home ready!");
+    console.log("Home ready123!");
     layoutAction.run_marquee();
     $("#entry").removeClass();
     $("#entry").css('margin-left', '9px');
